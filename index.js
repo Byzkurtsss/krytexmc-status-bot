d801aa526fd9d4794488036099d8f934c6e94157
Byzkurtsss <uc201032@gmail.com>
2026-02-07 23:12:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const mc = require('minecraft-server-util');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
 });
 
 const IP = "mc.hypixel.net";
 const PORT = 25565;
-const CHANNEL_ID = "KANAL_ID_BURAYA";
-
-let messageId = null;
+const CHANNEL_ID = "1469785789247389953";
 
 client.once('ready', async () => {
   console.log(`Bot aktif: ${client.user.tag}`);
@@ -29,22 +27,29 @@ client.once('ready', async () => {
         )
         .setTimestamp();
 
-      if (!messageId) {
-        const msg = await channel.send({ embeds: [embed] });
-        messageId = msg.id;
+      // Son 10 mesajda botun attığını bul
+      const messages = await channel.messages.fetch({ limit: 10 });
+      const botMsg = messages.find(m => m.author.id === client.user.id);
+
+      if (botMsg) {
+        await botMsg.edit({ embeds: [embed] });
       } else {
-        const msg = await channel.messages.fetch(messageId);
-        await msg.edit({ embeds: [embed] });
+        await channel.send({ embeds: [embed] });
       }
+
     } catch (e) {
       const embed = new EmbedBuilder()
         .setTitle("KrytexMC Sunucu Durumu")
         .addFields({ name: "Sunucu", value: "Kapalı" })
         .setTimestamp();
 
-      if (messageId) {
-        const msg = await channel.messages.fetch(messageId);
-        await msg.edit({ embeds: [embed] });
+      const messages = await channel.messages.fetch({ limit: 10 });
+      const botMsg = messages.find(m => m.author.id === client.user.id);
+
+      if (botMsg) {
+        await botMsg.edit({ embeds: [embed] });
+      } else {
+        await channel.send({ embeds: [embed] });
       }
     }
   }, 30000);
